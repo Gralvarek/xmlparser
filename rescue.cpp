d929978ce36a3943d1ca2fc42deae1f67d73b265
James <james.asghar@gmail.com>
2015-02-09 23:37:37 -0600

    Added in the Rescue class

---
+++ rescue.cpp
@@ -0,0 +1,38 @@
+class Rescue {
+  public:
+    vector<string> rescuer(fstream file, string line, string xmltag) 
+    {
+      // Initializes the rescuer
+      vector<string> rescuer;
+
+      // Checks to see if file was opened
+      if (file.is_open()) {
+        cout << "Successfully opened file." << endl;
+
+        // Iterates over each line in the file
+        while (getline(file, line)) {
+
+	  // Finds the tag at the beginning of the line
+	  if ((line.find(xmltag)) == 0) {
+
+	    // Rescues the trapped string!
+	    string trapped = line.substr(xmltag.length(), line.length() 
+				     - (xmltag.length() * 2 + 1));
+	    rescuer.push_back(trapped);
+	  }
+        }
+
+        // Test Only
+        for (unsigned i = 0; i < rescuer.size(); i++) {
+	  cout << rescuer.at(i) << endl;
+        }
+
+        // Closes file
+        file.close();
+      }
+      else cout << "Unable to open file." << endl;
+  
+      // Returns the rescued strings in a vector
+      return rescuer; 
+    }
+};
--- xmlparser.cpp
+++ xmlparser.cpp
@@ -2,43 +2,22 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "rescue.cpp"
 using namespace std;
 
 
-int main(int argc, char* argv[])
+int main()
 {
   // Initializes local variables
   fstream file("test.xml", ios::in | ios::binary);
   string line;
-  vector<string> rescuer;
+  vector<string> rescued;
+  Rescue resc;
 
   // Reads in xml tag
   string xmltag = "<Val>";
-
-  // Checks to see if file was opened
-  if (file.is_open()) {
-    cout << "Successfully opened file." << endl;
-
-    // Iterates over each line in the file
-    while (getline(file, line)) {
-
-      // Finds the tag at the beginning of the line
-      if ((line.find(xmltag)) == 0) {
-
-        // Rescues the trapped string!
-        string trapped = line.substr(xmltag.length(), line.length() - (xmltag.length() * 2 + 1));
-        rescuer.push_back(trapped);
-      }
-    }
-    // Test Only
-    for (unsigned i = 0; i < rescuer.size(); i++) {
-      cout << rescuer.at(i) << endl;
-    }
-
-    // Closes file
-    file.close();
-  }
-  else cout << "Unable to open file." << endl;
+  
+  rescued = resc.rescuer(file, line, xmltag);
 
   return 0;
 }
